fbeb2295920d07a38dd7ce27acbbcc2b0904fd1a
Bao Nguyen Thien <65020023+bao-nguyenbku@users.noreply.github.com>
2023-03-02 21:45:39 +0700

    update reservation dto

--- src/module/private/reservations/dto/create-reserve.dto.ts
+++ src/module/private/reservations/dto/create-reserve.dto.ts
@@ -1,4 +1,11 @@
-import { OmitType } from '@nestjs/swagger';
+import { PickType } from '@nestjs/swagger';
 import { ReservationEntity } from '../entities/reservation.entity';
 
-export class CreateReservationDto extends OmitType(ReservationEntity, ['id']) {}
+export class CreateReservationDto extends PickType(ReservationEntity, [
+  'numberOfGuest',
+  'from',
+  'to',
+]) {
+  customerId: string;
+  tableId: string;
+}
--- src/module/private/reservations/entities/reservation.entity.ts
+++ src/module/private/reservations/entities/reservation.entity.ts
@@ -1,14 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
 import mongoose from 'mongoose';
+import { TableEntity } from '../../tables/entities/table.entity';
+import { UserDataDto } from '~/module/common/users/dto/response-user';
+
 export class ReservationEntity {
   @ApiProperty()
   id: mongoose.Schema.Types.ObjectId;
 
   @ApiProperty()
-  owner: string;
+  owner: UserDataDto;
 
   @ApiProperty()
-  table: number;
+  table: TableEntity;
 
   @ApiProperty()
   numberOfGuest: number;
