c534b75607ee2b80df266e5c7a983f23bf3b1ca2
kogleo <hellresiak@gmail.com>
2023-05-29 02:38:06 +0700

    update user api

--- src/module/common/users/dto/update-user.dto.ts
+++ src/module/common/users/dto/update-user.dto.ts
@@ -1,12 +1,15 @@
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto {
+  @IsOptional()
   @IsString()
   fullname: string;
 
+  @IsOptional()
   @IsString()
   password: string;
 
+  @IsOptional()
   @IsString()
   phone: string;
 }
